5b6299ad536f7355d4b35eafdde1095e66fbf30f
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-11-24 16:03:13 +0530

    logo update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center space-x-2 group">
-            <img src="public/assests/nextgen.png" alt="NextGen Hiring" className="h-auto w-52"/>
+            <img src="/assests/nextgen.png" alt="NextGen Hiring" className="h-auto w-52"/>
             {/* <span className="text-xl font-semibold text-white">NextGen Hiring</span> */}
             
           </Link>
